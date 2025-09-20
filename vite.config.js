c66ccd0a13de0193f1c28283ce741384b6cd0011
Mani1002 <maniofrock@gmail.com>
2025-09-20 14:40:01 +0530

    complete

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/', // important for Netlify deployment at root
+  build: {
+    outDir: 'dist', // default Vite output folder
+  },
 })
